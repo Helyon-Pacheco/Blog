bcce4bf6095d5bef12350cbcfa5863dd0e7e03a4
Helyon-Pacheco <helyonftp@hotmail.com>
2022-10-25 16:48:54 -0300

    Iniciando Projeto

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
 
 namespace Blog
 {
     public class Program
     {
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=068678HFtp";
         public static void Main(string[] args)
         {
             Console.WriteLine();
+        }
+
+        public static void ReadUsers()
+        {
+            using(var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var users = connection.GetAll<User>();
+            }
         }
     }
 }
