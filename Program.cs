1ec48e41f3732c663087cc99c6ea38e32a2c2ced
Helyon-Pacheco <helyonftp@hotmail.com>
2022-10-25 15:31:29 -0300

    Commit Inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blog
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
