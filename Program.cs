2d50a9544a3a966fded2da8da964f92174739177
Helyon-Pacheco <helyonftp@hotmail.com>
2022-10-26 20:41:03 -0300

    Criando repositorio

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Blog.Models;
+using Blog.Repositories;
 using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 
@@ -10,24 +11,23 @@ namespace Blog
         private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=068678HFtp";
         public static void Main(string[] args)
         {
-            // ReadUsers();
+            var connection = new SqlConnection();
+            connection.Open();
+            ReadUsers(connection);
             // ReadUser();
             // CreateUser();
             // UpdateUser();
             // DeleteUser();
+            connection.Close();
         }
 
-        public static void ReadUsers()
+        public static void ReadUsers(SqlConnection connection)
         {
-            using(var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                var users = connection.GetAll<User>();
-
-                foreach( var user in users)
-                {
-                    Console.WriteLine(user.Name);
-                }
-            }
+            var repository = new UserRepository(connection);
+            var users = repository.Get();
+            
+            foreach( var user in users)
+                Console.WriteLine(user.Name);
         }
 
         public static void ReadUser()
