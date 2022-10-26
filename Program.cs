ed39e373490fadc7d933713b96af6c26cbe17f2a
Helyon-Pacheco <helyonftp@hotmail.com>
2022-10-26 19:31:46 -0300

    Adicionando funções de acesso ao bd

--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ namespace Blog
         private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=068678HFtp";
         public static void Main(string[] args)
         {
-            Console.WriteLine();
+            ReadUsers();
+            // ReadUser();
+            //CreateUser();
         }
 
         public static void ReadUsers()
@@ -18,6 +20,39 @@ namespace Blog
             using(var connection = new SqlConnection(CONNECTION_STRING))
             {
                 var users = connection.GetAll<User>();
+
+                foreach( var user in users)
+                {
+                    Console.WriteLine(user.Name);
+                }
+            }
+        }
+
+        public static void ReadUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+                Console.WriteLine(user.Name);
+            }
+        }
+
+        public static void CreateUser()
+        {
+            var user = new User()
+            {
+                Bio = "Equipe balta.io",
+                Email = "hello@balta.io",
+                Image = "https://...",
+                Name = "Equipe balta.io",
+                PasswordHash = "HASH",
+                Slug = "equipe-balta"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Insert<User>(user);
+                Console.WriteLine("Cadastro realizado com sucesso!");
             }
         }
     }
