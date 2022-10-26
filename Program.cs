38995e3ea7b18a29d50915858bb21ca009e256de
Helyon-Pacheco <helyonftp@hotmail.com>
2022-10-26 19:59:29 -0300

    Adicionando funções update e delete

--- Program.cs
+++ Program.cs
@@ -10,9 +10,11 @@ namespace Blog
         private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=068678HFtp";
         public static void Main(string[] args)
         {
-            ReadUsers();
+            // ReadUsers();
             // ReadUser();
-            //CreateUser();
+            // CreateUser();
+            // UpdateUser();
+            // DeleteUser();
         }
 
         public static void ReadUsers()
@@ -51,6 +53,37 @@ namespace Blog
 
             using (var connection = new SqlConnection(CONNECTION_STRING))
             {
+                connection.Insert<User>(user);
+                Console.WriteLine("Cadastro realizado com sucesso!");
+            }
+        }
+
+        public static void UpdateUser()
+        {
+            var user = new User()
+            {
+                Id = 2,
+                Bio = "Equipe | balta.io",
+                Email = "hello@balta.io",
+                Image = "https://...",
+                Name = "Equipe de suporte balta.io",
+                PasswordHash = "HASH",
+                Slug = "equipe-balta"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Update<User>(user);
+                Console.WriteLine("Cadastro atualizado com sucesso!");
+            }
+        }
+
+        public static void DeleteUser()
+        {
+            
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(2);
                 connection.Insert<User>(user);
                 Console.WriteLine("Cadastro realizado com sucesso!");
             }
